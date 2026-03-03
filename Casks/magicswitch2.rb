f7fcac3741dda84cc38f9a790079734174d590e6
martin-knap <96686504+martin-knap@users.noreply.github.com>
2026-03-03 17:53:30 +0100

    Add MagicSwitch2 cask v1.0.0

---
+++ Casks/magicswitch2.rb
@@ -0,0 +1,11 @@
+cask "magicswitch2" do
+  version "1.0.0"
+  sha256 "08a2cb09835f56713dacb8dfca367f635f545efaad50ee4ebea1e806771064cf"
+
+  url "https://github.com/martin-knap/homebrew-magic-switch2/releases/download/v#{version}/MagicSwitch2.app.zip"
+  name "MagicSwitch2"
+  desc "One-click Magic Keyboard and Trackpad switching between Macs"
+  homepage "https://github.com/martin-knap/homebrew-magic-switch2"
+
+  app "MagicSwitch2.app"
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# homebrew-magic-switch2
+
+Homebrew tap for installing MagicSwitch2.
+
+## Install
+
+```bash
+brew tap martin-knap/magic-switch2
+brew install --cask magicswitch2
+```
